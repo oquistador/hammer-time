e59074af91a5c96e7f47dcc997b8fdb4900e86ee
Todd Oquist <toquist@gmail.com>
2014-01-04 01:47:59 -0800

    building this out a little more

--- assets/scripts/main.coffee
+++ assets/scripts/main.coffee
@@ -1,8 +1,48 @@
-window.tataki = ((App)->
-	App.load = ->
+window.Hammer = ((App)->
+	App.data = 
+		[
+			{
+				id: 'brushfill'
+				name: 'Brush Fill'
+				image: 'drum.png'
+				audio: 'brushfill.120.mp3'
+			}
+			{
+				id: 'mellowrhodes'
+				name: 'Mellow Rhodes'
+				image: 'piano.png'
+				audio: 'mellowrhodes_Cmi_120.mp3'
+			}
+			{
+				id: 'sawtooth'
+				name: 'Sawtooth Wave'
+				image: 'sawtooth.png'
+				audio: '220_Hz_sawtooth_wave.ogg'
+			}
+		]
 
+	App.load = ->
+		manifest = @data.map (item)-> id: item.id, src: item.audio
+
+		@pendingAudioCount = @data.length
+		
+		createjs.Sound.alternateExtensions = ["mp3"]
+		
+		handleLoad = App.init.bind App
+
+		createjs.Sound.addEventListener 'fileload', handleLoad
+		createjs.Sound.registerManifest manifest, 'audio/'
+
+	App.init = (target)->
+		@pendingAudioCount--
+
+		$pad = $ document.createElement('div')
+		$pad.attr(id: target.id, class: 'pad')
+		$pad.on 'click', -> createjs.Sound.play @id
+
+		$('.pads').append $pad
 
 	App
-)(window.tataki or {})
+)(window.Hammer or {})
 
-window.tataki.load()
+window.Hammer.load()
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,50 @@
-window.tataki = (function(App) {
-  App.load = function() {};
+window.Hammer = (function(App) {
+  App.data = [
+    {
+      id: 'brushfill',
+      name: 'Brush Fill',
+      image: 'drum.png',
+      audio: 'brushfill.120.mp3'
+    }, {
+      id: 'mellowrhodes',
+      name: 'Mellow Rhodes',
+      image: 'piano.png',
+      audio: 'mellowrhodes_Cmi_120.mp3'
+    }, {
+      id: 'sawtooth',
+      name: 'Sawtooth Wave',
+      image: 'sawtooth.png',
+      audio: '220_Hz_sawtooth_wave.ogg'
+    }
+  ];
+  App.load = function() {
+    var handleLoad, manifest;
+    manifest = this.data.map(function(item) {
+      return {
+        id: item.id,
+        src: item.audio
+      };
+    });
+    this.pendingAudioCount = this.data.length;
+    createjs.Sound.alternateExtensions = ["mp3"];
+    handleLoad = App.init.bind(App);
+    createjs.Sound.addEventListener('fileload', handleLoad);
+    return createjs.Sound.registerManifest(manifest, 'audio/');
+  };
+  App.init = function(target) {
+    var $pad;
+    this.pendingAudioCount--;
+    $pad = $(document.createElement('div'));
+    $pad.attr({
+      id: target.id,
+      "class": 'pad'
+    });
+    $pad.on('click', function() {
+      return createjs.Sound.play(this.id);
+    });
+    return $('.pads').append($pad);
+  };
   return App;
-})(window.tataki || {});
+})(window.Hammer || {});
 
-window.tataki.load();
+window.Hammer.load();
