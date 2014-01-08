ff69f3fe08eec656b50c997ddcd39c6499394ac0
Todd Oquist <toquist@gmail.com>
2014-01-07 17:30:59 -0800

    work on sprites, loading, and sequencing mechanics

--- assets/scripts/main.coffee
+++ assets/scripts/main.coffee
@@ -1,10 +1,12 @@
 window.Hammer = ((App)->
 	App.config = 
-		targetFPS: 40
+		bpm: 120
+		resolution: 1/8
 		sprite:
 			width: 200
 			height: 200
 			numFrames: 4
+			fps: 16
 		paths: 
 			sprite: 'images/sprites/'
 			audio: 'audio/'
@@ -14,26 +16,41 @@ window.Hammer = ((App)->
 			{
 				id: 'black'
 				sprite: 'hexagon_black.png'
-				audio: 'button-1.mp3'
+				audio: 'shuffled_house_120.mp3'
+				soloDuration: 4
+				row: 0
+				col: 0
 			}
 			{
 				id: 'blue'
 				sprite: 'hexagon_blue.png'
 				audio: 'button-2.mp3'
+				soloDuration: 1/8
+				row: 0
+				col: 1
 			}
 			{
 				id: 'green'
 				sprite: 'hexagon_green.png'
 				audio: 'button-3.mp3'
+				soloDuration: 1/8
+				row: 0
+				col: 2
 			}
 			{
 				id: 'red'
 				sprite: 'hexagon_red.png'
 				audio: 'button-4.mp3'
+				soloDuration: 1/8
+				row: 0
+				col: 3
 			}
 		]
 
 	window.AudioContext = window.AudioContext or window.webkitAudioContext
+	window.requestAnimationFrame = window.requestAnimationFrame or window.webkitRequestAnimationFrame
+
+	App.canvas = {}
 
 	App.audio = 
 		context: new AudioContext()
@@ -46,16 +63,10 @@ window.Hammer = ((App)->
 		remainingItems = totalItems
 		loaderBar = document.querySelector '#loader .bar'
 
-		xhr = (src, callback)->
-			req = new XMLHttpRequest()
-			req.open 'GET', src, true
-			req.responseType = 'arraybuffer'
-
-			req.addEventListener 'load', ->
-				callback req.response
-				updateLoader()
-
-			req.send()
+		@canvas.el = document.querySelector 'canvas'
+		@canvas.el.width = window.innerWidth
+		@canvas.el.height = window.innerHeight
+		@canvas.ctx = @canvas.el.getContext '2d'
 
 		updateLoader = ->
 			remainingItems--
@@ -63,49 +74,76 @@ window.Hammer = ((App)->
 			
 			unless remainingItems
 				setTimeout (->
-						document.body.removeChild document.querySelector('#loader')
-						App.init()
+						loader = document.querySelector('#loader')
+						transitionEnd = (evt)->
+							return unless evt.propertyName is 'opacity'
+						
+							document.body.removeChild loader
+							App.init()
+
+						loader.style.opacity = 0
+
+						loader.addEventListener 'transitioned', transitionEnd
+						loader.addEventListener 'webkitTransitionEnd', transitionEnd
 					), 500
 
 
 		loadAudio = (item)->
 			ctx = App.audio.context
 
-			xhr "#{App.config.paths.audio}#{item.audio}", (res)->
-				ctx.decodeAudioData res, (buffer)->
+			req = new XMLHttpRequest()
+			req.open 'GET', "#{App.config.paths.audio}#{item.audio}", true
+			req.responseType = 'arraybuffer'
+
+			req.addEventListener 'load', ->
+				ctx.decodeAudioData req.response, (buffer)->
 					App.audio.sounds[item.id] = new App.Sound buffer
+					updateLoader()				
+
+			req.send()
 
 		loadImage = (item)->
-			xhr "#{App.config.paths.sprite}#{item.sprite}", (res)->
-				blob = new Blob [res], type: 'image/png'
-				App.sprites[item.id] = new App.Sprite blob
+			image = document.createElement 'img'
+			image.addEventListener 'load', ->
+				opts =
+					image: image
+					col: item.col
+					row: item.row
 
-		for item in @manifest
+				App.sprites[item.id] = new App.Sprite opts
+
+				updateLoader()
+
+			image.src = "#{App.config.paths.sprite}#{item.sprite}"
+
+		@manifest.forEach (item)->
 			loadAudio item
 			loadImage item
 
 	App.init = ->
 		pads = []
-		pads.push( new App.Pad item.id ) for item in @manifest
+		@manifest.forEach (item)-> pads.push( new App.Pad item )
 
 		@state = new App.State pads
 
+		@canvas.el.addEventListener 'click', ->
+			App.state.pads[0].trigger()
+			App.state.pads[2].trigger()
+
 	class App.State
 		constructor: (@pads)->
-			canvas = document.createElement 'canvas'
-			canvas.width = window.innerWidth
-			canvas.height = window.innerHeight
+			@update()
 
-			document.body.appendChild canvas
+		update: (ts)=>
+			if ts
+				App.canvas.ctx.clearRect 0, 0, App.canvas.el.width, App.canvas.el.height
+				@pads.forEach (pad)-> pad.update ts
 
-			@initPads()
+			requestAnimationFrame @update
 
-			@time = 0
+			return
 
-		update: (evt)->
-			@stage.update()
-
-		initPads: ->
+		render: (dt)->
 
 	class App.Sound
 		constructor: (@buffer)->
@@ -118,13 +156,79 @@ window.Hammer = ((App)->
 			source.start time
 
 	class App.Sprite
-		constructor: (@blob)->
+		@prototype.fps = App.config.sprite.fps
+		@prototype.numFrames = App.config.sprite.numFrames
+		@prototype.length = (1000 / @prototype.fps) * @prototype.numFrames
+		@prototype.width = App.config.sprite.width
+		@prototype.height = App.config.sprite.height
+
+		constructor: (opts)->
+			@image = opts.image
+			@x = opts.col * @width
+			@y = opts.row * @height
+			@reset()
+
+		play: (@timeRemaining = 0)->
+			@isPlaying = true
+			@index = 1
+			@
+
+		reset: ->
+			@isPlaying = false
+			@duration = 0
+			@index = 0
+			@
+
+		update: (dt)->
+			if @isPlaying
+				@timeRemaining -= dt
+
+				if @timeRemaining > 0
+					@index = (@index + dt * @fps / 1000) % @numFrames
+				else 
+					@reset()
+			@
+
+		render: (dt)->
+			@update dt
+
+			App.canvas.ctx.save()
+			App.canvas.ctx.drawImage @image, Math.floor(@index) * @width, 0, @width, @height, @x, @y, @width, @height
+			App.canvas.ctx.restore()
+			@
 
 	class App.Pad
-		constructor: (@id)->
+		@prototype.resolution = 60 / App.config.bpm * App.config.resolution * 4 * 1000
+		constructor: (opts)->
+			@id = opts.id
 			@sound = App.audio.sounds[@id]
 			@sprite = App.sprites[@id]
-	
+			
+			@spriteDuration = Math.floor(@sound.buffer.duration * 1000 / @sprite.length) * @sprite.length
+			@spriteDuration = 500 unless @spriteDuration
+
+			@duration = 
+			
+			@queue = []
+
+		update: (ts)->
+			@ts = ts unless @ts
+			
+			dt = ts - @ts
+			@ts = ts
+
+			if @triggerAt and @ts >= @triggerAt
+				@sound.play(0)
+				@sprite.play(@spriteDuration)
+				
+				@triggerAt = null
+			
+			@sprite.render(dt)
+
+		trigger: ->
+			unless @triggerAt or @sprite.isPlaying
+				@triggerAt = @ts + @resolution - (@ts % @resolution)
+
 	App
 )(window.Hammer or {})
 
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,14 @@
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
 window.Hammer = (function(App) {
   App.config = {
-    targetFPS: 40,
+    bpm: 120,
+    resolution: 1 / 8,
     sprite: {
       width: 200,
       height: 200,
-      numFrames: 4
+      numFrames: 4,
+      fps: 16
     },
     paths: {
       sprite: 'images/sprites/',
@@ -15,107 +19,134 @@ window.Hammer = (function(App) {
     {
       id: 'black',
       sprite: 'hexagon_black.png',
-      audio: 'button-1.mp3'
+      audio: 'shuffled_house_120.mp3',
+      soloDuration: 4,
+      row: 0,
+      col: 0
     }, {
       id: 'blue',
       sprite: 'hexagon_blue.png',
-      audio: 'button-2.mp3'
+      audio: 'button-2.mp3',
+      soloDuration: 1 / 8,
+      row: 0,
+      col: 1
     }, {
       id: 'green',
       sprite: 'hexagon_green.png',
-      audio: 'button-3.mp3'
+      audio: 'button-3.mp3',
+      soloDuration: 1 / 8,
+      row: 0,
+      col: 2
     }, {
       id: 'red',
       sprite: 'hexagon_red.png',
-      audio: 'button-4.mp3'
+      audio: 'button-4.mp3',
+      soloDuration: 1 / 8,
+      row: 0,
+      col: 3
     }
   ];
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame;
+  App.canvas = {};
   App.audio = {
     context: new AudioContext(),
     sounds: {}
   };
   App.sprites = {};
   App.load = function() {
-    var item, loadAudio, loadImage, loaderBar, remainingItems, totalItems, updateLoader, xhr, _i, _len, _ref, _results;
+    var loadAudio, loadImage, loaderBar, remainingItems, totalItems, updateLoader;
     totalItems = App.manifest.length * 2;
     remainingItems = totalItems;
     loaderBar = document.querySelector('#loader .bar');
-    xhr = function(src, callback) {
-      var req;
-      req = new XMLHttpRequest();
-      req.open('GET', src, true);
-      req.responseType = 'arraybuffer';
-      req.addEventListener('load', function() {
-        callback(req.response);
-        return updateLoader();
-      });
-      return req.send();
-    };
+    this.canvas.el = document.querySelector('canvas');
+    this.canvas.el.width = window.innerWidth;
+    this.canvas.el.height = window.innerHeight;
+    this.canvas.ctx = this.canvas.el.getContext('2d');
     updateLoader = function() {
       remainingItems--;
       loaderBar.style.width = "" + (Math.ceil(((totalItems - remainingItems) / totalItems) * 100)) + "%";
       if (!remainingItems) {
         return setTimeout((function() {
-          document.body.removeChild(document.querySelector('#loader'));
-          return App.init();
+          var loader, transitionEnd;
+          loader = document.querySelector('#loader');
+          transitionEnd = function(evt) {
+            if (evt.propertyName !== 'opacity') {
+              return;
+            }
+            document.body.removeChild(loader);
+            return App.init();
+          };
+          loader.style.opacity = 0;
+          loader.addEventListener('transitioned', transitionEnd);
+          return loader.addEventListener('webkitTransitionEnd', transitionEnd);
         }), 500);
       }
     };
     loadAudio = function(item) {
-      var ctx;
+      var ctx, req;
       ctx = App.audio.context;
-      return xhr("" + App.config.paths.audio + item.audio, function(res) {
-        return ctx.decodeAudioData(res, function(buffer) {
-          return App.audio.sounds[item.id] = new App.Sound(buffer);
+      req = new XMLHttpRequest();
+      req.open('GET', "" + App.config.paths.audio + item.audio, true);
+      req.responseType = 'arraybuffer';
+      req.addEventListener('load', function() {
+        return ctx.decodeAudioData(req.response, function(buffer) {
+          App.audio.sounds[item.id] = new App.Sound(buffer);
+          return updateLoader();
         });
       });
+      return req.send();
     };
     loadImage = function(item) {
-      return xhr("" + App.config.paths.sprite + item.sprite, function(res) {
-        var blob;
-        blob = new Blob([res], {
-          type: 'image/png'
-        });
-        return App.sprites[item.id] = new App.Sprite(blob);
+      var image;
+      image = document.createElement('img');
+      image.addEventListener('load', function() {
+        var opts;
+        opts = {
+          image: image,
+          col: item.col,
+          row: item.row
+        };
+        App.sprites[item.id] = new App.Sprite(opts);
+        return updateLoader();
       });
+      return image.src = "" + App.config.paths.sprite + item.sprite;
     };
-    _ref = this.manifest;
-    _results = [];
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      item = _ref[_i];
+    return this.manifest.forEach(function(item) {
       loadAudio(item);
-      _results.push(loadImage(item));
-    }
-    return _results;
+      return loadImage(item);
+    });
   };
   App.init = function() {
-    var item, pads, _i, _len, _ref;
+    var pads;
     pads = [];
-    _ref = this.manifest;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      item = _ref[_i];
-      pads.push(new App.Pad(item.id));
-    }
-    return this.state = new App.State(pads);
+    this.manifest.forEach(function(item) {
+      return pads.push(new App.Pad(item));
+    });
+    this.state = new App.State(pads);
+    return this.canvas.el.addEventListener('click', function() {
+      App.state.pads[0].trigger();
+      return App.state.pads[2].trigger();
+    });
   };
   App.State = (function() {
     function State(pads) {
-      var canvas;
       this.pads = pads;
-      canvas = document.createElement('canvas');
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-      document.body.appendChild(canvas);
-      this.initPads();
-      this.time = 0;
+      this.update = __bind(this.update, this);
+      this.update();
     }
 
-    State.prototype.update = function(evt) {
-      return this.stage.update();
+    State.prototype.update = function(ts) {
+      if (ts) {
+        App.canvas.ctx.clearRect(0, 0, App.canvas.el.width, App.canvas.el.height);
+        this.pads.forEach(function(pad) {
+          return pad.update(ts);
+        });
+      }
+      requestAnimationFrame(this.update);
     };
 
-    State.prototype.initPads = function() {};
+    State.prototype.render = function(dt) {};
 
     return State;
 
@@ -137,19 +168,94 @@ window.Hammer = (function(App) {
 
   })();
   App.Sprite = (function() {
-    function Sprite(blob) {
-      this.blob = blob;
+    Sprite.prototype.fps = App.config.sprite.fps;
+
+    Sprite.prototype.numFrames = App.config.sprite.numFrames;
+
+    Sprite.prototype.length = (1000 / Sprite.prototype.fps) * Sprite.prototype.numFrames;
+
+    Sprite.prototype.width = App.config.sprite.width;
+
+    Sprite.prototype.height = App.config.sprite.height;
+
+    function Sprite(opts) {
+      this.image = opts.image;
+      this.x = opts.col * this.width;
+      this.y = opts.row * this.height;
+      this.reset();
     }
+
+    Sprite.prototype.play = function(timeRemaining) {
+      this.timeRemaining = timeRemaining != null ? timeRemaining : 0;
+      this.isPlaying = true;
+      this.index = 1;
+      return this;
+    };
+
+    Sprite.prototype.reset = function() {
+      this.isPlaying = false;
+      this.duration = 0;
+      this.index = 0;
+      return this;
+    };
+
+    Sprite.prototype.update = function(dt) {
+      if (this.isPlaying) {
+        this.timeRemaining -= dt;
+        if (this.timeRemaining > 0) {
+          this.index = (this.index + dt * this.fps / 1000) % this.numFrames;
+        } else {
+          this.reset();
+        }
+      }
+      return this;
+    };
+
+    Sprite.prototype.render = function(dt) {
+      this.update(dt);
+      App.canvas.ctx.save();
+      App.canvas.ctx.drawImage(this.image, Math.floor(this.index) * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+      App.canvas.ctx.restore();
+      return this;
+    };
 
     return Sprite;
 
   })();
   App.Pad = (function() {
-    function Pad(id) {
-      this.id = id;
+    Pad.prototype.resolution = 60 / App.config.bpm * App.config.resolution * 4 * 1000;
+
+    function Pad(opts) {
+      this.id = opts.id;
       this.sound = App.audio.sounds[this.id];
       this.sprite = App.sprites[this.id];
+      this.spriteDuration = Math.floor(this.sound.buffer.duration * 1000 / this.sprite.length) * this.sprite.length;
+      if (!this.spriteDuration) {
+        this.spriteDuration = 500;
+      }
+      this.duration = this.queue = [];
     }
+
+    Pad.prototype.update = function(ts) {
+      var dt;
+      if (!this.ts) {
+        this.ts = ts;
+      }
+      dt = ts - this.ts;
+      this.ts = ts;
+      if (this.triggerAt && this.ts >= this.triggerAt) {
+        this.sound.play(0);
+        this.sprite.play(this.spriteDuration);
+        this.triggerAt = null;
+      }
+      return this.sprite.render(dt);
+    };
+
+    Pad.prototype.trigger = function() {
+      if (!(this.triggerAt || this.sprite.isPlaying)) {
+        return this.triggerAt = this.ts + this.resolution - (this.ts % this.resolution);
+      }
+    };
 
     return Pad;
 
